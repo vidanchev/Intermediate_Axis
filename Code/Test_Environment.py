e6c7a6f4c3afbd5f98d6c2ff3eb787c79eefe045
Victor Danchev <spectrefdx@gmail.com>
2021-10-21 00:40:46 +0300

    Started a basic description in LaTeX, added a basic 3D view

--- Code/Test_Environment.py
+++ Code/Test_Environment.py
@@ -4,43 +4,79 @@ from RK_Driver import rk_step
 from Parameters_and_Constants import pi
 import matplotlib.pyplot as plt
 
-# Initial body attitude with respect to inertial frame [-]
-q_init = [ 1.0 , 0.0 , 0.0 , 0.0 ]
+# Make a 2D plot of the results for the angular velocity
+# Inputs:
+# - Time array (time[ N ]) [sec]
+# - Angular rate array (om_arr[ N ][ 3 ]) [rad/s]
+# - fig-name:
+# -- "0" if no figure is to be saved
+# -- string which is the fig name otherwise
+def plot_2D( time , om_arr , fig_name ):
 
-# Initial body angular velocity in body-frame [rad/s]
-om_init = [ pi/10.0 , pi/2.0 , pi/10.0 ]
+    # Plot the results
+    fig, ax = plt.subplots()
 
-npoints = 1000 # Number of points for integrator [-]
-time_tot = 120.0 # Simulation time [sec]
+    ax.plot( time , np.transpose( om_arr )[ 0 ] , linestyle = "solid" , label = r"\omega_x" )
+    ax.plot( time , np.transpose( om_arr )[ 1 ] , linestyle = "solid" , label = r"\omega_y" )
+    ax.plot( time , np.transpose( om_arr )[ 2 ] , linestyle = "solid" , label = r"\omega_z" )
 
-time = np.linspace( 0 , time_tot , npoints ) # Create time array
-dt = time_tot/( npoints - 1.0 ) # Get the number of intervals for integration
+    plt.xlabel( "Time (s)" )
+    plt.ylabel( "Angular Rate [rad/s]" )
+    plt.title( "Intermediate Axis Plot" )
 
-# Create arrays where to keep the attitude and angular rate data for plotting
-q_arr = np.zeros( ( npoints , 4 ) )
-om_arr = np.zeros( ( npoints , 3 ) )
+    plt.grid()
 
-# Populate the arrays with the initial values
-q_arr[ 0 ] = q_init
-om_arr[ 0 ] = om_init
+    if fig_name != "0":
+        fig.savefig( fig_name + ".png" , format = "png" )
 
-# Propagate for each of the points
-for i in range( 0 , npoints - 1 ):
-    [ q_arr[ i + 1 ] , om_arr[ i + 1 ] ] = rk_step( [ q_arr[ i ] , om_arr[ i ] ] , time[ i ] , dt )
+    plt.show( )
 
-# Plot the results
-fig, ax = plt.subplots()
 
-ax.plot( time , np.transpose( om_arr )[ 0 ] , linestyle = "solid" , label = r"\omega_x" )
-ax.plot( time , np.transpose( om_arr )[ 1 ] , linestyle = "solid" , label = r"\omega_y" )
-ax.plot( time , np.transpose( om_arr )[ 2 ] , linestyle = "solid" , label = r"\omega_z" )
+# Make a 3D animation of the results for the angular velocity
+# Inputs:
+# - Time array (time[ N ]) [sec]
+# - Angular rate array (om_arr[ N ][ 3 ]) [rad/s]
+def animate_3D( time , om_arr ):
 
-plt.xlabel( "Time (s)" )
-plt.ylabel( "Angular Rate [rad/s]" )
-plt.title( "Intermediate Axis Plot" )
+    ax = plt.figure( ).add_subplot( projection = "3d" )    
+    
+    for i in range( 0 , len( time ) ):
+        plt.cla( )
+        ax.quiver( 0 , 0 , 0 , om_arr[ i ][ 0 ] , om_arr[ i ][ 1 ] , om_arr[ i ][ 2 ] , normalize = True )
 
-plt.grid()
+        ax.set_xlim3d( -1.2 , 1.2 )
+        ax.set_ylim3d( -1.2 , 1.2 )
+        ax.set_zlim3d( -1.2 , 1.2 )
 
-fig.savefig( "2D_Plot.png" )
+        plt.pause( 0.1 )
 
-plt.show()
+# To include 3D motion visualization of the 3-axis system -> body frame
+
+if __name__ == "__main__":
+
+    # Initial body attitude with respect to inertial frame [-]
+    q_init = [ 1.0 , 0.0 , 0.0 , 0.0 ]
+
+    # Initial body angular velocity in body-frame [rad/s]
+    om_init = [ pi/10.0 , pi/2.0 , pi/10.0 ]
+
+    npoints = 1000 # Number of points for integrator [-]
+    time_tot = 120.0 # Simulation time [sec]
+
+    time = np.linspace( 0 , time_tot , npoints ) # Create time array
+    dt = time_tot/( npoints - 1.0 ) # Get the number of intervals for integration
+
+    # Create arrays where to keep the attitude and angular rate data for plotting
+    q_arr = np.zeros( ( npoints , 4 ) )
+    om_arr = np.zeros( ( npoints , 3 ) )
+
+    # Populate the arrays with the initial values
+    q_arr[ 0 ] = q_init
+    om_arr[ 0 ] = om_init
+
+    # Propagate for each of the points
+    for i in range( 0 , npoints - 1 ):
+        [ q_arr[ i + 1 ] , om_arr[ i + 1 ] ] = rk_step( [ q_arr[ i ] , om_arr[ i ] ] , time[ i ] , dt )
+    
+    #plot_2D( time , om_arr , "2D_Plot" )
+    animate_3D( time , om_arr )
--- README.md
+++ README.md
@@ -4,10 +4,23 @@ Have you ever seen a [T-handle spinning in microgravity?](https://www.youtube.co
 
 Most people's reaction: :open_mouth:
 
-# Purpose of this repo
+## Purpose of this repo
 
-I first saw this video in 2015 when I was a 1st year Bachelor student. 
-I had learned enough to describe it mathematically and got a perturbative solution in 2016. 
-I then made an Octave script to solve it numerically in 2018. 
+I first saw this video in 2015 when I was a 1st year Bachelor student.
+I had learned enough to describe it mathematically and got a perturbative solution in 2016.
+I then made an Octave script to solve it numerically in 2018.
 This repo's purpose is to rewrite this code in Python and provide it to others who are interested in the phenomenology of the process.
 I will also add mathematical description and perturbative description to be used for comparison with the numerical results!
+
+## Organization
+
+- **Code** contains all the code (written in Python) which simulates the phenomenon.
+It is split into several .py files as follows:
+  - **Parameters_and_Constrants** includes all the physical parameters and inertial properties of the body to be simulated;
+  - **RK_Driver** contains several functions which compute the right-hand side of the dynamical differential equations and drives numerical integration through a classical Runge-Kutta (RK4) integrator;
+  - **Visualizations** contains some different visualization functions (either as 2D plots or time-dependent 3D animation with a vector);
+  - **Test_Environment** is used for general testing of different functions and for a driver of the integration + plotting;
+  - **Perturbative_Test** is used to check between the analytical solution (small deviation) and the numerical results obtained by the integrator - it is a verification that the code works in small offset from the intermediate axis;
+- **Physics** contains a LaTeX document with the explanation of some of the basic concepts, it contains a general mathematical description of the rotary motion of a body in 3 dimensions and solves the differential equations perturbatively in the case of a small deviation from the intermediate axis.
+
+## Running the code for the T-Handle scenario
